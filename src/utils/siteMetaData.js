c5e2d30770c18de05520857f5db50f64d8672db7
ritesh <riteshf7@gmail.com>
2025-08-08 11:26:48 +0530

    added personal info

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,20 +1,20 @@
 // please update required information
 const siteMetadata = {
-    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-    author: 'CodeBucks',
-    headerTitle: 'Next.js Blog',
-    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    title: 'Android Blog By Ritesh',
+    author: 'Ritesh Singh',
+    headerTitle: 'Android Blog',
+    description: 'A blog spot for android developers',
     language: 'en-us',
     theme: 'system', // system, dark or light
     siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
-    facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
+    email: 'iamritesh.dev@gmail.com', 
+    github: 'https://www.github.com/riteshf7',
+    twitter: 'https://twitter.com/riteshf7',
+    facebook: 'https://facebook.com/riteshsingh201',
+    youtube: 'https://youtube.com/dotrythisathome',
+    linkedin: 'https://www.linkedin.com/in/riteshf1',
     dribbble: 'https://www.dribbble.com',
     locale: 'en-US',
   }
