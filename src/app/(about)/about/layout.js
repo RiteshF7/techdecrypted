608ca6a3b684bdf706a06875090af585743f66f2
ritesh <riteshf7@gmail.com>
2025-08-08 16:34:59 +0530

    added

---
+++ src/app/(about)/about/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "About Me",
+  description: "About Ritesh Singh - Technology professional with over 7 years of experience",
+};
+
+export default function AboutLayout({ children }) {
+  return children;
+}
